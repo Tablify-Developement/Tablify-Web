2d7b58e208924d6d4dc11a41da0a38aecd020dd9
Jones Cian <cian.code@hotmail.com>
2025-03-28 13:21:45 +0100

    Revert "Feature home page"

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
-import {Header} from "@/components/HomePage/Header";
+import "../styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,19 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-          >
-              <Header />
-            {children}
-          </ThemeProvider>
+        {children}
       </body>
     </html>
   );
